7c3a1c830cc3759b38f4cac3548ad48a1f294dcf
Adarsh <adarsh232002@gmail.com>
2024-10-02 01:18:47 +0530

    SearchUI Complete

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Adarsh Singh on 01/10/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
